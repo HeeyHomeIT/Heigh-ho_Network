4da5477afc4a6dad3969ffb222b166d451837be6
PC-201611271801\Administrator <yaohong@heeyhome.com>
2016-12-09 13:00:47 +0800

    用户个人中心修改

--- heeyhome_web/src/js/j_customer/app_customer.js
+++ heeyhome_web/src/js/j_customer/app_customer.js
@@ -1,9 +1,9 @@
 /**
  * Created by Administrator on 2016/11/23.
  */
-define(['angular', 'angular-resource', 'angular-route'], function (angular) {
+define(['angular', 'angular-resource', 'angular-route', 'ChineseDistricts', 'distpicker', 'pagination'], function (angular) {
 
-    var app = angular.module('customerApp', ['ngResource', 'ngRoute']);
+    var app = angular.module('customerApp', ['ngResource', 'ngRoute', 'tm.pagination']);
 
     app.init = function () {
         angular.bootstrap(document, ['customerApp']);
@@ -24,7 +24,17 @@ define(['angular', 'angular-resource', 'angular-route'], function (angular) {
 
 
      });*/
-    app.config(['$routeProvider', '$controllerProvider', function ($routeProvider, $controllerProvider) {
+    app.config(["$provide", "$compileProvider", "$controllerProvider", "$filterProvider",
+        function ($provide, $compileProvider, $controllerProvider, $filterProvider) {
+            app.controller = $controllerProvider.register;
+            app.directive = $compileProvider.directive;
+            app.filter = $filterProvider.register;
+            app.factory = $provide.factory;
+            app.service = $provide.service;
+            app.constant = $provide.constant;
+        }]);
+
+    app.config(['$routeProvider', '$controllerProvider', '$httpProvider', function ($routeProvider, $controllerProvider, $httpProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'view/v_customer_user/customer_first.html'
@@ -42,15 +52,15 @@ define(['angular', 'angular-resource', 'angular-route'], function (angular) {
                 }
             })
 
-             .when('/my_order', {
-                    templateUrl: 'view/v_customer_user/my_order.html',
-                    controller: 'my_order',
-                    resolve: {
-                    	// ctrlRegister为我自己写的一个复用的函数，
-                        // 用于注入控制器。见第三部分
-                        delay: ctrlRegister('my_order', ['js/j_customer/my_order'])
-                    }
-                })
+            .when('/my_order', {
+                templateUrl: 'view/v_customer_user/my_order.html',
+                controller: 'my_order',
+                resolve: {
+                    // ctrlRegister为我自己写的一个复用的函数，
+                    // 用于注入控制器。见第三部分
+                    delay: ctrlRegister('my_order', ['js/j_customer/my_order'])
+                }
+            })
 
             .when('/my_collection', {
                 templateUrl: 'view/v_customer_user/my_collection.html',
@@ -96,6 +106,7 @@ define(['angular', 'angular-resource', 'angular-route'], function (angular) {
         }
 
     }]);
+
 
     app.run(function () {
         //run some code here ...
