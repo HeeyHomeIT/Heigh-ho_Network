258114ac9af9988d449b8fabbb8044e246004a13
pjw <980682473@qq.com>
2016-12-14 13:08:43 +0800

    工长个人店铺详情API

---
+++ doc/API文档/店铺详情API.md
@@ -0,0 +1,84 @@
+# ShopdetailController #
+# index() #
+## 店铺详情信息接口
+
+
+### 接口地址
+
+
+```
+.../shopinfo
+```
+
+### 接口格式
+
+### 调用
+
+```
+接收方式        GET
+```
+
+```
+.../shopinfo
+```
+
+###### Json数据格式
+```
+data
+shop_id             shop_id
+
+callback            callback
+```
+
+### 回调
+###### Json数据格式
+
+```
+成功
+callback(
+code         000
+data         {
+                    shopper_id           shopper_id             //工长id
+                    shop_name            shop_name              //店铺名
+                    authentication       authentication 数组    //店铺认证  1:保证金 2:团队保险 3:平台实名认证 4:两年质保维修
+                    opentime             opentime               //开店时间   
+                    servicetag           servicetag 数组        //擅长风格
+                    servicearea          servicearea 数组       //服务区域
+                    shop_workernum       shop_workernum         //施工团队
+                    shop_describe        shop_describe          //店铺理念(签名)
+                    shop_imgs            {
+                                               id               id
+                                               shop_img         shop_img
+                                               is_face          is_face
+                    shopper_info         {
+                                               foremaninfo_realname     foremaninfo_realname
+                                                home_province           home_province
+                                                home_city               home_city
+                                                worktime                worktime           //工龄
+                                                experience              experience         //从业经历
+                                                decoratedareas          decoratedareas     //装修小区
+                                                portrait_img            portrait_img       //工长头像
+                                                
+                                         }
+                                         
+             }
+msg          ""
+)
+```
+
+```
+失败
+callback(
+code          111
+data          ""
+msg           失败
+)
+```
+
+###### Code值含义
+
+```
+000           成功
+112           店铺id不能为空
+111           店铺不存在
+```
---
+++ heeyhome_webapi/heeyhomeapi/app/Http/Controllers/ShopdetailController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: heeyhome
+ * Date: 2016/12/14
+ * Time: 10:00
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\DB;
+
+class ShopdetailController extends Controller
+{
+    public function index(){
+        $callback=rq('callback');
+        $shop_id=rq('shop_id');
+        $shop_info=DB::select('select shopper_id,shop_name,authentication,opentime,servicetag,servicearea,shop_address,shop_describe,shop_workernum from hh_shop where shop_id=?',[$shop_id]);
+        $authentication=explode(',',$shop_info[0]->authentication);
+        $servicetag=explode(',',$shop_info[0]->servicetag);
+        $servicearea=explode(',',$shop_info[0]->servicearea);
+        //dd($authentication);
+        foreach($authentication as $k=>$v){
+            switch ($v){
+                case 1: $authentication[$k]='api/public/smrz.png';
+                    break;
+                case 2: $authentication[$k]='api/public/bzj.png';
+                    break;
+                case 3: $authentication[$k]='api/public/tdbx.png';
+                    break;
+                case 4: $authentication[$k]='api/public/bx.png';
+                    break;
+                default: $authentication[$k]='';
+                    break;
+            }
+        }
+        $shop_info[0]->authentication=$authentication;
+        $shop_info[0]->servicetag=$servicetag;
+        $shop_info[0]->servicearea=$servicearea;
+        if($shop_info){
+            $update = DB::update('update hh_shop set shop_scan=shop_scan+1 where shop_id=?', [$shop_id]);
+            $shop_img=DB::select('select id,shop_img,is_face from hh_shop_img where shop_id=?',[$shop_id]);
+            $shop_info[0]->shop_imgs=$shop_img;
+            $shopper_info=DB::select('select a.foremaninfo_realname,a.home_province,a.home_city,a.worktime,a.experience,a.decoratedareas,b.portrait_img from hh_foremaninfo a left join hh_portrait b on b.portrait_userid=a.foremaninfo_userid where a.foremaninfo_userid=?',[$shop_info[0]->shopper_id]);
+            $shop_info[0]->shopper_info=$shopper_info[0];
+            $arr = array("code" => "111",
+                "data"=>$shop_info[0]
+            );
+            return $callback . "(" . HHJson($arr) . ")";
+        }else{
+            $arr = array("code" => "111",
+                "msg"=>"该店铺不存在"
+            );
+            return $callback . "(" . HHJson($arr) . ")";
+        }
+    }
+}
