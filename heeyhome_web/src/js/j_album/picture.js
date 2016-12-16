445ddbc6a12255c7240a437475f5d6cc79761d37
WIN-TJB6IR9U6EF\Administrator <caikk@heeyhome.com>
2016-12-16 17:42:59 +0800

    工艺相册提交

---
+++ heeyhome_web/src/js/j_album/picture.js
@@ -0,0 +1,62 @@
+var selectKey = "1";
+var picList = [{
+	"picPos": 1,
+	"pid": "1540637",
+	"bigPic": "image/bigPic/1.jpg",
+	"bigPic": "image/bigpic/1.jpg",
+	"thumbPic": "image/thumbPic/1.jpg"
+}, {
+	"picPos": 2,
+	"pid": "1520876",
+	"bigPic": "image/bigPic/2.jpg",
+	"bigPic": "image/bigPic/2.jpg",
+	"thumbPic": "image/thumbPic/2.jpg"
+}, {
+	"picPos": 3,
+	"pid": "1520550",
+	"bigPic": "image/bigPic/3.jpg",
+	"bigPic": "image/bigPic/3.jpg",
+	"thumbPic": "image/thumbPic/3.jpg"
+}, {
+	"picPos": 4,
+	"pid": "1520549",
+	"bigPic": "image/bigPic/4.jpg",
+	"bigPic": "image/bigPic/4.jpg",
+	"thumbPic": "image/thumbPic/4.jpg"
+}, {
+	"picPos": 5,
+	"pid": "1520548",
+	"bigPic": "image/bigPic/5.jpg",
+	"bigPic": "image/bigPic/5.jpg",
+	"thumbPic": "image/thumbPic/5.jpg"
+}, {
+	"picPos": 6,
+	"pid": "1520547",
+	"bigPic": "image/bigPic/6.jpg",
+	"bigPic": "image/bigPic/6.jpg",
+	"thumbPic": "image/thumbPic/6.jpg"
+}, {
+	"picPos": 7,
+	"pid": "1520546",
+	"bigPic": "image/bigPic/7.jpg",
+	"bigPic": "image/bigPic/7.jpg",
+	"thumbPic": "image/thumbPic/7.jpg"
+}, {
+	"picPos": 8,
+	"pid": "1520545",
+	"bigPic": "image/bigPic/8.jpg",
+	"bigPic": "image/bigPic/8.jpg",
+	"thumbPic": "image/thumbPic/8.jpg"
+}, {
+	"picPos": 9,
+	"pid": "1520544",
+	"bigPic": "image/bigPic/9.jpg",
+	"bigPic": "image/bigPic/9.jpg",
+	"thumbPic": "image/thumbPic/9.jpg"
+}, {
+	"picPos": 10,
+	"pid": "1520543",
+	"bigPic": "image/bigPic/10.jpg",
+	"bigPic": "image/bigPic/10.jpg",
+	"thumbPic": "image/thumbPic/10.jpg"
+}];
