0d94c96dac5c43f466b9469e508af33e502e7c38
PC-201611271801\Administrator <yaohong@heeyhome.com>
2016-12-21 13:00:14 +0800

    工长个人中心修改

--- heeyhome_web/src/js/j_common/common.js
+++ heeyhome_web/src/js/j_common/common.js
@@ -1,99 +1,106 @@
-define(['app','base64','cookie'], function(app) {
-	(function() {
-		/* 定义一个类 */
-		var headWrap = {
-			/* 入口方法 */
-			init: function() {
-				/**
-				 * 业务代码
-				 */
-				headWrap.initEvent();
-			},
-			initEvent: function() {
-				var self = this;
-				self.searchBoxClickEvent(); // 搜索框点击事件
-				self.myHeeyHomeEvent(); // 我的嘿吼
-				self.getUserDataEvent(); // 得到登录信息
-			},
-			/**
-			 * 搜索框点击事件
-			 */
-			searchBoxClickEvent: function() {
-				$("#c_search_text").on({
-					"focus": function() {
-						var $this = $(this);
-						var placeholder = $this.attr("attr-placeholder");
-						if($this.val() == placeholder) {
-							$this.val("");
-						}
-						$(".c_searchpop").show();
-					},
-					"blur": function() {
-						var $this = $(this);
-						var placeholder = $this.attr("attr-placeholder");
-						if($this.val() == '') {
-							$this.val(placeholder);
-						}
-						$(".c_searchpop").hide();
-					}
-				});
-			},
-			/**
-			 * 我的嘿吼
-			 */
-			myHeeyHomeEvent: function() {
-				$("#c_myhh").hover(function() {
-					$(this).children("a").addClass("item_hover_180");
-					$(this).children("div").css("height", "112px");
-					$(this).addClass("open");
-				}, function() {
-					$(this).children("a").removeClass("item_hover_180");
-					$(this).children("div").css("height", "0px");
-					$(this).removeClass("open");
-				});
-			},
-			/**
-			 * 登录信息
-			 */
-			getUserDataEvent: function() {
-				var userName = $.cookie('userName');
-				var cloneLoginStr = $("#topLogin").html(); // 克隆未登录前页面模块
-				var cloneLgStr = $(".userinfo p").html()
-				if(userName != null && userName != "") {
-					var loginStr = '<a class="user_information" rel="nofollow" href="center.html#/center" >' + $.base64.decode(userName) + '</a><span>，您好 </span><span class="exit">退出</span>';
-					var lgStr = '<a class="uinfo" href="#nogo"><span>' + $.base64.decode(userName) + '</span></a>'
-					cloneHtmlHendler.loginClone(loginStr, lgStr);
-					$(".exit").on("click", function() { // 点击退出的事件
-						cloneHtmlHendler.loginClone(cloneLoginStr, cloneLgStr);
-						$.cookie("userId", null, {
-							expires: -1,
-							path: '/'
-						});
-						$.cookie("userName", null, {
-							expires: -1,
-							path: '/'
-						});
-						$.cookie("userType", null, {
-							expires: -1,
-							path: '/'
-						});
-					});
-				}
+define(['app', 'base64', 'cookie'], function (app) {
+    (function () {
+        /* 定义一个类 */
+        var headWrap = {
+            /* 入口方法 */
+            init: function () {
+                /**
+                 * 业务代码
+                 */
+                headWrap.initEvent();
+            },
+            initEvent: function () {
+                var self = this;
+                self.searchBoxClickEvent(); // 搜索框点击事件
+                self.myHeeyHomeEvent(); // 我的嘿吼
+                self.getUserDataEvent(); // 得到登录信息
+            },
+            /**
+             * 搜索框点击事件
+             */
+            searchBoxClickEvent: function () {
+                $("#c_search_text").on({
+                    "focus": function () {
+                        var $this = $(this);
+                        var placeholder = $this.attr("attr-placeholder");
+                        if ($this.val() == placeholder) {
+                            $this.val("");
+                        }
+                        $(".c_searchpop").show();
+                    },
+                    "blur": function () {
+                        var $this = $(this);
+                        var placeholder = $this.attr("attr-placeholder");
+                        if ($this.val() == '') {
+                            $this.val(placeholder);
+                        }
+                        $(".c_searchpop").hide();
+                    }
+                });
+            },
+            /**
+             * 我的嘿吼
+             */
+            myHeeyHomeEvent: function () {
+                $("#c_myhh").hover(function () {
+                    $(this).children("a").addClass("item_hover_180");
+                    $(this).children("div").css("height", "112px");
+                    $(this).addClass("open");
+                }, function () {
+                    $(this).children("a").removeClass("item_hover_180");
+                    $(this).children("div").css("height", "0px");
+                    $(this).removeClass("open");
+                });
+            },
+            /**
+             * 登录信息
+             */
+            getUserDataEvent: function () {
+                var userName = $.cookie('userName');
+                var userType = $.cookie('userType');
+                var cloneLoginStr = $("#topLogin").html(); // 克隆未登录前页面模块
+                var cloneLgStr = $(".userinfo p").html();
+                if (userName != null && userName != "") {
+                    /* 判断是工长登录还是用户登录 */
+                    if ($.base64.decode(userType) == 1){
+                        var loginStr = '<a class="user_information" rel="nofollow" href="center.html#/center" >' + $.base64.decode(userName) + '</a><span>，您好 </span><span class="exit">退出</span>';
+                    }else if($.base64.decode(userType) == 2){
+                        var loginStr = '<a class="user_information" rel="nofollow" href="master.html#/master" >' + $.base64.decode(userName) + '</a><span>，您好 </span><span class="exit">退出</span>';
+                    }
 
-			}
-		};
-		/**
-		 * 删除追加代码
-		 */
-		cloneHtmlHendler = {
-			loginClone: function(str1, str2) {
-				$("#topLogin").empty().append(str1);
-				$(".userinfo p").empty().append(str2);
-			}
-		};
-		// 入口方法调用
-		app.headerWrapHandler = function() {
-			headWrap.init();
-		}
-	})();
+                    var lgStr = '<a class="uinfo" href="#nogo"><span>' + $.base64.decode(userName) + '</span></a>';
+                    cloneHtmlHendler.loginClone(loginStr, lgStr);
+                    $(".exit").on("click", function () { // 点击退出的事件
+                        cloneHtmlHendler.loginClone(cloneLoginStr, cloneLgStr);
+                        $.cookie("userId", null, {
+                            expires: -1,
+                            path: '/'
+                        });
+                        $.cookie("userName", null, {
+                            expires: -1,
+                            path: '/'
+                        });
+                        $.cookie("userType", null, {
+                            expires: -1,
+                            path: '/'
+                        });
+                    });
+                }
+
+            }
+        };
+        /**
+         * 删除追加代码
+         */
+        cloneHtmlHendler = {
+            loginClone: function (str1, str2) {
+                $("#topLogin").empty().append(str1);
+                $(".userinfo p").empty().append(str2);
+            }
+        };
+        // 入口方法调用
+        app.headerWrapHandler = function () {
+            headWrap.init();
+        }
+    })();
 });
