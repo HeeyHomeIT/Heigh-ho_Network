a09542eb3bd477c56fdb44f4681531a58f9ec467
PC-201611271801\Administrator <yaohong@heeyhome.com>
2017-01-19 11:21:08 +0800

    身份验证

--- heeyhome_web/src/js/j_center/safe.js
+++ heeyhome_web/src/js/j_center/safe.js
@@ -219,7 +219,52 @@
                 // });
 
                 //$("#if").load(function () {
-                function upload() {
+               
+                $.ajax({
+                    url: AUTHURL,
+                    type: "GET",
+                    async: true,
+                    dataType: 'jsonp',
+                    data: {
+                        user_id: $.base64.decode($.cookie("userId"))
+                    },
+                    success: function (data) {
+                        console.log(data);
+                        if (data.code == '130') {//正在审核中
+                            $('.revise_process').remove();
+                            $('.bind_phonecnt').remove();
+                            $('#new_info').append('<div id="face" class="showImg fl"><img src=""> </div><!--showImg--><div id="back" class="showImg fl"><img src=""></div><!--showImg--><div class="deal_wrap clearfix fl"> <div class="deal_left fl"> <i class="iconfont">&#xe67c;</i> </div><!--deal_left--> <div class="deal_right fl"> <p class="process">身份正在验证中...</p> <p class="wait">请耐心等待</p> </div><!--deal_left--> </div><!--deal_wrap-->');
+                            $('.content_wrap').css('height', '898px');
+                            $('#face img').attr('src', 'http://www.heeyhome.com/' + data.data.facephoto);
+                            $('#back img').attr('src', 'http://www.heeyhome.com/' + data.data.backphoto);
+                        } else if (data.code == '131') {//审核未通过
+                            $('.revise_process').hide();
+                            $('.bind_phonecnt').hide();
+                            $('#new_info').append('<div id="face" class="showImg fl"><img src=""> </div><!--showImg--><div id="back" class="showImg fl"><img src=""></div><!--showImg--><div class="deal_wrap clearfix fl"> <div class="deal_left fl"> <i class="iconfont">&#xe625;</i> </div><!--deal_left--> <div class="deal_right fl"> <p class="process">您的审核尚未通过！</p> <p class="wait">请点击<a id="new_upload" href="javascript:;">重新上传信息~~</a></p> </div><!--deal_left--> </div><!--deal_wrap-->');
+                            $('.content_wrap').css('height', '898px');
+                            $('#face img').attr('src', 'http://www.heeyhome.com/' + data.data.facephoto);
+                            $('#back img').attr('src', 'http://www.heeyhome.com/' + data.data.backphoto);
+                        } else if (data.code == '000') {//审核通过
+                            $('.revise_process').remove();
+                            $('.bind_phonecnt').remove();
+                            $('#new_info').append('<div id="face" class="showImg fl"><img src=""> </div><!--showImg--><div id="back" class="showImg fl"><img src=""></div><!--showImg--><div class="deal_wrap clearfix fl"> <div class="deal_left fl"> <i class="iconfont">&#xe615;</i> </div><!--deal_left--> <div class="deal_right fl"> <p class="process complete_process">您的审核已通过！</p></div><!--deal_left--> </div><!--deal_wrap-->');
+                            $('.content_wrap').css('height', '898px');
+                            $('#face img').attr('src', 'http://www.heeyhome.com/' + data.data.facephoto);
+                            $('#back img').attr('src', 'http://www.heeyhome.com/' + data.data.backphoto);
+                        }
+                    },
+                    error: function (data) {
+                    }
+                });
+
+
+                $(document).on('click', '#new_upload', function () {
+                    $('#new_info').remove();
+                    $('.revise_process').show();
+                    $('.bind_phonecnt').show();
+                });
+
+                $(document).on('click', '.next_confirm', function () {
                     $.ajax({
                         url: AUTHURL,
                         type: "GET",
@@ -244,28 +289,20 @@
                                 $('.content_wrap').css('height', '898px');
                                 $('#face img').attr('src', 'http://www.heeyhome.com/' + data.data.facephoto);
                                 $('#back img').attr('src', 'http://www.heeyhome.com/' + data.data.backphoto);
-                            }else if (data.code == '000') {//审核通过
+                            } else if (data.code == '000') {//审核通过
                                 $('.revise_process').remove();
                                 $('.bind_phonecnt').remove();
                                 $('#new_info').append('<div id="face" class="showImg fl"><img src=""> </div><!--showImg--><div id="back" class="showImg fl"><img src=""></div><!--showImg--><div class="deal_wrap clearfix fl"> <div class="deal_left fl"> <i class="iconfont">&#xe615;</i> </div><!--deal_left--> <div class="deal_right fl"> <p class="process complete_process">您的审核已通过！</p></div><!--deal_left--> </div><!--deal_wrap-->');
                                 $('.content_wrap').css('height', '898px');
                                 $('#face img').attr('src', 'http://www.heeyhome.com/' + data.data.facephoto);
                                 $('#back img').attr('src', 'http://www.heeyhome.com/' + data.data.backphoto);
+                            } else {
+                                layer.alert('图片上传出错');
                             }
                         },
                         error: function (data) {
                         }
                     });
-                }
-                upload();
-                $(document).on('click','#new_upload',function () {
-                    $('#new_info').remove();
-                    $('.revise_process').show();
-                    $('.bind_phonecnt').show();
-                });
-
-                $(document).on('click','.next_confirm',function () {
-                    upload();
                 });
                 //});
             }]);
