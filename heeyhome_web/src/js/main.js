118be924c555af61c36eb298231b60f372a4fe2a
HEEYHOME-PC\HEEYHOME <tangj@heeyhome.com>
2017-01-24 15:48:58 +0800

    BUG修改

--- heeyhome_web/src/js/main.js
+++ heeyhome_web/src/js/main.js
@@ -111,7 +111,7 @@
      * init main
      * 自动导入app.js,index.js模块
      */
-    require(['app', 'route', 'directive', 'cropbox', 'pagination', 'paging', 'dialog', 'ymdClass', 'layer','idCode'], function (app) {
+    require(['app', 'route', 'directive', 'cropbox', 'pagination', 'paging', 'dialog', 'ymdClass', 'layer','idCode','cookie'], function (app) {
         // 手动启动angularjs，特别说明此处的bootstrap不是那个ui框架，而是angularjs的一个手动启动框架的函数
         app.init();
     })
