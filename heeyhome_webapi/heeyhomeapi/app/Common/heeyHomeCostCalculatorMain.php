bc8ba51bed2611591977ccb68a805819c09c6d09
heeyhome-PC\heeyhome <it@heeyhome.com>
2017-02-22 12:59:57 +0800

    主材公式填入CostCalculatorMain种

--- heeyhome_webapi/heeyhomeapi/app/Common/heeyHomeCostCalculatorMain.php
+++ heeyhome_webapi/heeyhomeapi/app/Common/heeyHomeCostCalculatorMain.php
@@ -5,3 +5,66 @@
  * Date: 2017/2/13
  * Time: 16:19
  */
+
+
+function czdd($flag, $room_ground_first, $room_ground_second)
+{
+    switch ($flag) {
+        case 1:
+            $B5 = 0;
+            $D5 = 0;
+            return;
+        case 2:
+            return;
+        case 3:
+            return;
+        case 4:
+            return;
+        case 5:
+            return;
+        case 6:
+            return;
+        case 7:
+            return;
+    }
+}
+
+function czgd()
+{
+}
+
+function bc()
+{
+}
+
+function dls()
+{
+}
+
+function db()
+{
+}
+
+function mm()
+{
+}
+
+function cfym()
+{
+}
+
+function lyfym()
+{
+}
+
+function ygym()
+{
+}
+
+function jcdd()
+{
+}
+
+function cgsys()
+{
+}
