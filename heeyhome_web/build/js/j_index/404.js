a09857a486166c066ca257e518c348f0c6afd07e
HEEYHOME-PC\HEEYHOME <tangj@heeyhome.com>
2017-03-29 18:24:36 +0800

    计算器修改压缩上传

--- heeyhome_web/build/js/j_index/404.js
+++ heeyhome_web/build/js/j_index/404.js
@@ -1,2 +1,2 @@
-/*! Pc.Heigh-ho.Renovation-1.0.0.js 2017-03-23 */
+/*! Pc.Heigh-ho.Renovation-1.0.0.js 2017-03-29 */
 define(["app"],function(a){!function(){var b={init:function(){b.initEvent()},initEvent:function(){var a=this;a.initErrorEvent()},initErrorEvent:function(){$(".error_detail .error_button .back").on("click",function(){history.back()})}};a.errorWrapHandler=function(){b.init()}}()});
